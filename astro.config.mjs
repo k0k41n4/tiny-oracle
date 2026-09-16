3c77e16fd56c978db074d46172f197cb871cc1a9
esa <kresna.maheswara02@gmail.com>
2026-09-16 16:25:09 +0800

    I hope this last update
    update again to show

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 import alpinejs from '@astrojs/alpinejs';
 
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()]
   },
