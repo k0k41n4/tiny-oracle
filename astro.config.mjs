bfd9c93cf1dbe0442459d8c413ca4a914c03fa70
esa <kresna.maheswara02@gmail.com>
2026-09-16 12:11:35 +0800

    Update Cloudflare Integration to Git
    Hehehhehehehheh

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 import alpinejs from '@astrojs/alpinejs';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()]
   },
